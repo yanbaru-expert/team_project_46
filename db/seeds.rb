1f0f713663b33a2d68c053930c0d9adc2f8808a1
takurofukuta <mojuraspax@icloud.com>
2021-03-27 09:51:14 +0000

    seed.rbの管理者ユーザーの作成部分を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+ADMIN_EMAIL = 'admin@example.com'
 EMAIL = 'test@example.com'
 PASSWORD = 'password'
 
@@ -8,4 +9,7 @@ end
 
 movie_path = "db/csv_data/movie_data.csv"
 ImportCsv.movie_data(movie_path)
-AdminUser.find_or_create_by!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
+  admin_user.password = PASSWORD
+  puts "管理者ユーザーの初期データインポートに成功しました。"
+end
