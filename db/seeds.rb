4a37e7718d0b0c9b4b83cff5a0d236ed5698c8da
takurofukuta <mojuraspax@icloud.com>
2021-04-15 03:41:59 +0000

    admin/user.rbからcurrent_sign_in_at , sign_in_countカラムを削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,11 @@ User.find_or_create_by!(email: EMAIL) do |user|
   puts 'ユーザーの初期データインポートに成功しました。'
 end
 
-movie_path = "db/csv_data/movie_data.csv"
-ImportCsv.movie_data(movie_path)
 AdminUser.find_or_create_by!(email: ADMIN_EMAIL) do |admin_user|
   admin_user.password = PASSWORD
   puts "管理者ユーザーの初期データインポートに成功しました。"
 end
+
+movie_path = "db/csv_data/movie_data.csv"
+ImportCsv.movie_data(movie_path)
+
